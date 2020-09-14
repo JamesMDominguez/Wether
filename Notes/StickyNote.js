e97c52521f0e5462904c8ef23dbcdc69a0651849
James Dominguez <69368392+JamesMDominguez@users.noreply.github.com>
2020-09-14 15:39:54 -0700

    Add files via upload

---
+++ Notes/StickyNote.js
@@ -0,0 +1,16 @@
+var divtest = document.createElement("div");
+
+ function myFunction() {
+  let objTo = document.getElementById('box')
+  let textarea = document.createElement("textarea")
+
+    objTo.appendChild(divtest)
+    divtest.appendChild(textarea)
+
+}
+  
+
+
+
+//9b8b92e7c92b9bc99e377333293601ea
+
